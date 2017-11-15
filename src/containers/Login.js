8e7232cc95f2fe16903922c37fe384eec7734ca2
sungliky <sungliky@gmail.com>
2017-11-15 17:09:59 +0900

    login screen view set

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import { Authentication } from '../components/index';
 
 export default class Login extends Component {
   render() {
     return (
       <div>
-        Login
+        <Authentication mode={true}/>
       </div>
     )
   }
--- src/containers/Register.js
+++ src/containers/Register.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import { Authentication } from '../components/index';
 
 export default class Register extends Component {
   render() {
     return (
       <div>
-        Register
+        <Authentication mode = {false} />
       </div>
     )
   }
